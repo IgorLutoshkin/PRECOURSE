5b29b84d3979f14279bc05e43aa1760b28ae82fc
IgorLutoshkin <ig.yarij@mail.ru>
2024-01-06 13:18:26 +0300

    add 06-01-2024

--- index3.js
+++ index3.js
@@ -1,64 +1,103 @@
 let userName = {
-	name: 'Dima'
-}
-
+  name: "Dima",
+};
 
 let card1 = {
-	type: 'debit',
-	networkType: 'mastercard',
-	currencyType: 'USD',
-	balance: 5.75053,
-	number: 5282345678901289,
-	expirationYear: 2025,
-	expirationMonth: 9,
-	isActive: true,
-	isBlocked: false
+  type: "debit",
+  networkType: "mastercard",
+  currencyType: "USD",
+  balance: 5.75053,
+  number: 5282345678901289,
+  expirationYear: 2025,
+  expirationMonth: 9,
+  isActive: true,
+  isBlocked: false,
+  transaction: {
+    title: "Order Revenue",
+    amount: 874,
+    date: new Date(2023, 3, 16, 25, 43, 22),
+  },
 };
 
 let card2 = {
-	type: 'credit',
-	networkType: 'visa',
-	currencyType: 'EUR',
-	balance: 1.34353,
-	number: 9232654321012032,
-	expirationYear: 2026,
-	expirationMonth: 4,
-	isActive: false,
-	isBlocked: true
+  type: "credit",
+  networkType: "visa",
+  currencyType: "EUR",
+  balance: 1.34353,
+  number: 9232654321012032,
+  expirationYear: 2026,
+  expirationMonth: 4,
+  isActive: false,
+  isBlocked: true,
+  transaction: {
+    title: "Order Revenue",
+    amount: 874,
+    date: new Date(2023, 3, 16, 25, 43, 22),
+  },
 };
 
 let card3 = {
-	type: 'credit',
-	networkType: 'visa',
-	currencyType: 'EUR',
-	balance: 1.34353,
-	number: 9232654321012032,
-	expirationYear: 2026,
-	expirationMonth: 4,
-	isActive: false,
-	isBlocked: true
+  type: "credit",
+  networkType: "visa",
+  currencyType: "EUR",
+  balance: 1.34353,
+  number: 9232654321012032,
+  expirationYear: 2026,
+  expirationMonth: 4,
+  isActive: false,
+  isBlocked: true,
+  transaction: {
+    title: "Order Revenue",
+    amount: 874,
+    date: new Date(2023, 3, 16, 25, 43, 22),
+  },
+
+  transactions: [
+    {
+      title: "Order Revenue",
+      amount: 874,
+      date: new Date(2023, 3, 16, 25, 43, 22),
+    },
+    {
+      title: "Order Revenue",
+      amount: 874,
+      date: new Date(2023, 3, 16, 25, 43, 22),
+    },
+  ],
 };
 // render is 'for'
-let cards = [card1, card2, card3]
+let cards = [card1, card2, card3];
 for (let i = 0; i < cards.length; i++) {
-	const card = cards[i];
-	document.write('<h2>Welcome Back, ', `${userName.name}`, '!', '</h2>')
-	document.write('<h3>My cards</h3>')
+  const card = cards[i];
+  document.write("<h2>Welcome Back, ", `${userName.name}`, "!", "</h2>");
+  document.write("<h3>My cards</h3>");
 
-	document.write('<div>')
+  document.write("<div>");
 
-	document.write('<h2>', card.type + ' ' + 'Card', '</h2>')
-	document.write('<h3>', card.networkType, '</h3>')
-	document.write('<h3>', 'Current Balanse', '</h3>')
-	document.write('<p>', card.balance, '</p>')
-	document.write('<p>', card.number, '</p>')
-	document.write('<p>', card.expirationMonth + '/' + card.expirationYear, '</p>')
-	document.write('<br>')
-	document.write('<h2>', 'History Transaction', '</h2>')
-	document.write('<>')
+  document.write("<h2>", card.type + " " + "Card", "</h2>");
+  document.write("<h3>", card.networkType, "</h3>");
+  document.write("<h3>", "Current Balanse", "</h3>");
+  document.write("<p>", card.balance, "</p>");
+  document.write("<p>", card.number, "</p>");
+  document.write(
+    "<p>",
+    card.expirationMonth + "/" + card.expirationYear,
+    "</p>"
+  );
+  document.write("<h2>", "History Transaction", "</h2>");
+  document.write(
+    "<ul>",
+    "<li>",
+    card.transaction.title,
+    ", ",
+    card.transaction.amount,
+    ", ",
+    card.transaction.date,
+    "</li>",
 
-	document.write('</>')
-
+    "</ul>"
+  );
+  document.write("<hr>");
 }
 
 // render card 1
