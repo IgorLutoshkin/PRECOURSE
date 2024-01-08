dc84ff39244bc916e9c29f9287a93282827d1f5d
IgorLutoshkin <ig.yarij@mail.ru>
2024-01-09 02:14:08 +0300

    add function lesson 1 05.12.23

--- precourse-05-01-2024/player.js
+++ precourse-05-01-2024/player.js
@@ -27,14 +27,16 @@ const playerList = {
   ],
 };
 
+// render
+renderPlaylist(playerList);
 function renderPlaylist(playerListRender) {
-
-  
   // title
-  const playListTitleEl = document.createElement("h1"); // создаем тег в новой переменной
+  renderPlaylistHeader(playerListRender); /* func in  renderPlaylist*/
+
+  /* const playListTitleEl = document.createElement("h1"); // создаем тег в новой переменной
   playListTitleEl.append(playerList.title); // в тег помещаем наш тайтл из объекта плээйлист
   document.body.append(playListTitleEl); // добавляем тег с тайтлом в тело боди
-
+ */
   // img in the playlist
   const playListCoverEl = document.createElement("img");
   playListCoverEl.src = playerList.coverImageUrl;
@@ -46,8 +48,13 @@ function renderPlaylist(playerListRender) {
   document.body.append(playerListWrapTracks);
 
   //add track1
+  // renderTrack(playerListRender.tracks[0], playerListWrapTracks);
 
-  const trackWrap1 = document.createElement("li"); // в нее сложим картинку и трек
+  /* автоматизируем добавлени трека в плейлист */
+
+  playerListRender.tracks.map((el) => renderTrack(el, playerListWrapTracks));
+}
+/*  const trackWrap1 = document.createElement("li"); // в нее сложим картинку и трек
   playerListWrapTracks.append(trackWrap1);
 
   const titleTracks1 = document.createElement("h5");
@@ -63,11 +70,11 @@ function renderPlaylist(playerListRender) {
   const playerListTrackFileWrap = document.createElement("audio");
   playerListTrackFileWrap.controls = true;
   playerListTrackFileWrap.src = playerList.tracks[0].trackFileUrl;
-  trackWrap1.append(playerListTrackFileWrap);
+  trackWrap1.append(playerListTrackFileWrap); */
 
-  //add track2
+//add track2
 
-  const trackWrap2 = document.createElement("li"); // в нее сложим картинку и трек
+/*   const trackWrap2 = document.createElement("li"); // в нее сложим картинку и трек
   playerListWrapTracks.append(trackWrap2);
 
   const titleTracks2 = document.createElement("h5");
@@ -84,7 +91,38 @@ function renderPlaylist(playerListRender) {
   playerListTrackFileWrap2.controls = true;
   playerListTrackFileWrap2.src = playerList.tracks[1].trackFileUrl;
   trackWrap2.append(playerListTrackFileWrap2);
+} */
+
+/* 
+    renderPlaylistHeader(playlistForRendering);
+    renderTrack(playlistForRendering.tracks[0]);
+    renderTrack(playlistForRendering.tracks[1]); */
+
+function renderPlaylistHeader(inputPlaylistForRendering) {
+  // здесь логика отрисовки шапки "входного" плейлиста
+  const playListTitleEl = document.createElement("h1"); // создаем тег в новой переменной
+  playListTitleEl.append(inputPlaylistForRendering.title); // в тег помещаем наш тайтл из объекта плээйлист
+  document.body.append(playListTitleEl); // добавляем тег с тайтлом в тело боди
 }
 
-// render
-renderPlaylist(playerList);
+function renderTrack(inputTrackForRendering, listWrap) {
+  //listWrap этот параметр для того чтобы наш код с треком добавился посредством этого параметра в документ
+  // inputTrackForRendering параметр в котоый мы положим далее как аргумент параметр родительской функции и элемент с индексом
+  // здесь логика отрисовки "входного" трека
+  const trackWrap = document.createElement("li"); // в нее сложим картинку и трек
+  listWrap.append(trackWrap);
+
+  const titleTracks = document.createElement("h5");
+  titleTracks.innerText = `${inputTrackForRendering.artistName} - ${inputTrackForRendering.trackTitle}`;
+  trackWrap.append(titleTracks);
+
+  const playerListTrackCoverImageUrl = document.createElement("img");
+  playerListTrackCoverImageUrl.src = inputTrackForRendering.trackCoverImageUrl;
+  playerListTrackCoverImageUrl.width = "50";
+  trackWrap.append(playerListTrackCoverImageUrl); //добавляем элемент в div без document пишется
+
+  const playerListTrackFileWrap = document.createElement("audio");
+  playerListTrackFileWrap.controls = true;
+  playerListTrackFileWrap.src = inputTrackForRendering.trackFileUrl;
+  trackWrap.append(playerListTrackFileWrap);
+}
