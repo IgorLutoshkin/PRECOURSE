56abbd6aebd06f26f1d653a7cece10d2c86430e5
IgorLutoshkin <ig.yarij@mail.ru>
2024-01-24 23:41:14 +0300

    fixed the addresses URL

--- precourse-05-01-2024/player/src/data.js
+++ precourse-05-01-2024/player/src/data.js
@@ -1,7 +1,7 @@
 export const playerList = [
   {
     title: "My playlist",
-    coverImageUrl: "/precourse-05-01-2024/player/CoverPlayList_1.png",
+    coverImageUrl: "/player/img/CoverPlayList_1.png",
     totalInfo: {
       totalTrackCount: 4,
       totalTrackDurationInSec: 733,
@@ -9,19 +9,19 @@ export const playerList = [
     tracks: [
       {
         tracksId: "11",
-        trackCoverImageUrl: "/precourse-05-01-2024/player/Eminem.png",
+        trackCoverImageUrl: "/player/img/Eminem.png",
         artistName: "Eminem",
         trackTitle: "Rap God",
-        trackFileUrl: "/precourse-05-01-2024/player/Eminem - Rap God.mp3",
+        trackFileUrl: "/player/audio/Eminem - Rap God.mp3",
         trackDurationInSec: 1,
         isHot: true,
       },
       {
         tracksId: "12",
-        trackCoverImageUrl: "/precourse-05-01-2024/player/50Cent.png",
+        trackCoverImageUrl: "/player/img/50Cent.png",
         artistName: "50cent",
         trackTitle: "In da club",
-        trackFileUrl: "/precourse-05-01-2024/player/50 Cent - In Da Club.mp3",
+        trackFileUrl: "/player/audio/50 Cent - In Da Club.mp3",
         trackDurationInSec: 1,
         isHot: true,
       },
@@ -29,7 +29,7 @@ export const playerList = [
   },
   {
     title: "My playlist",
-    coverImageUrl: "/precourse-05-01-2024/player/CoverPlayList_2.png",
+    coverImageUrl: "/player/img/CoverPlayList_2.png",
     totalInfo: {
       totalTrackCount: 4,
       totalTrackDurationInSec: 733,
@@ -37,19 +37,19 @@ export const playerList = [
     tracks: [
       {
         tracksId: "11",
-        trackCoverImageUrl: "/precourse-05-01-2024/player/Eminem.png",
+        trackCoverImageUrl: "/player/img/Eminem.png",
         artistName: "Eminem",
         trackTitle: "Rap God",
-        trackFileUrl: "/precourse-05-01-2024/player/Eminem - Rap God.mp3",
+        trackFileUrl: "/player/audio/Eminem - Rap God.mp3",
         trackDurationInSec: 1,
         isHot: true,
       },
       {
         tracksId: "12",
-        trackCoverImageUrl: "/precourse-05-01-2024/player/50Cent.png",
+        trackCoverImageUrl: "/player/img/50Cent.png",
         artistName: "50cent",
         trackTitle: "In da club",
-        trackFileUrl: "/precourse-05-01-2024/player/50 Cent - In Da Club.mp3",
+        trackFileUrl: "/player/audio/50 Cent - In Da Club.mp3",
         trackDurationInSec: 1,
         isHot: true,
       },
